3cd90a74889b10be90686061729000e1332b43e4
abhigyan <rookhallow31143@gmail.com>
2022-06-23 01:27:23 +0530

    Some more misc problems

---
+++ Misc/plusOne.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int carry=0,i;
+        for(i = digits.size()-1; i >=0; i--){
+            if(digits[i]==9){
+                digits[i]=0;
+                carry=1;
+            }
+            else{
+                digits[i]++;
+                carry=0;
+                break;
+            }
+        }
+        //cout << i << endl;
+        if(i==-1 && carry==1){
+            digits.insert(digits.begin(),1);
+        }
+        return digits;
+    }
+};
