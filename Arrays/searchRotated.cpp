01896310d651320c0588882fa6a779ae1469237a
abhigyan <rookhallow31143@gmail.com>
2022-07-15 11:00:36 +0530

    made it possible to run more files

--- Arrays/searchRotated.cpp
+++ Arrays/searchRotated.cpp
@@ -1,37 +1,50 @@
 #include<vector>
+#include<iostream>
 using namespace std;
-class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        int l=0,h=nums.size()-1,mid;
-        //cout << "h" << h << endl;
-        while(l<=h){
-            mid = l+(h-l)/2;
-            //cout << "mid" << mid << endl;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]>=nums[l]){
-                if(nums[mid]>=target && target>=nums[l]){
-                    h=mid-1;
-                    //cout << "h" << h << endl;
-                }
-                else{
-                    l=mid+1;
-                    //cout << "l" << l << endl;
-                }
+ // } Driver Code Ends
+class Solution{
+    public:
+    int search(vector<int> A, int l, int h, int key){
+        if(l>h){
+            return -1;
+        }
+        int find = l+(h-l)/2;
+        if(A[find]==key){
+            return find;
+        }
+        else if(A[find]>=A[l]){
+            if(A[find]>=key && key>=A[l]){
+                return search(A,l,find-1,key);
             }
             else{
-                if(nums[mid]<=target && target<=nums[h]){
-                    l=mid+1;
-                    //cout << "l" << l << endl;
-                }
-                else{
-                    h=mid-1;
-                    //cout << "h" << h << endl;
-                }
+                return search(A,find+1,h,key);
             }
         }
-        return -1;
+        else{
+            if(A[find]<=key && key<=A[h]){
+                return search(A,find+1,h,key);
+            }
+            else{
+                return search(A,l,find-1,key);
+            }
+        }
     }
 };
+
+// { Driver Code Starts. 
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> A(n);
+        for(int i = 0; i < n; i++)
+            cin >> A[i];
+        int key;
+        cin >> key;
+        Solution ob;
+        cout << ob.search(A, 0, n - 1, key) << endl;
+    }
+return 0;
+}  // } Driver Code Ends
